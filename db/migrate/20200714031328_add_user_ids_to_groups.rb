1f7a9cb558a5c4e5362aaf27286a00a3732a0f87
nbrmci <cr3886165@gmail.com>
2020-07-24 13:35:31 +0900

    edit file

--- db/migrate/20200714031328_add_user_ids_to_groups.rb
+++
@@ -1,5 +0,0 @@
-class AddUserIdsToGroups < ActiveRecord::Migration[6.0]
-  def change
-    add_column :groups, :user_ids, :integer
-  end
-end
--- db/migrate/20200714031826_remove_user_ids_from_groups.rb
+++
@@ -1,5 +0,0 @@
-class RemoveUserIdsFromGroups < ActiveRecord::Migration[6.0]
-  def change
-    remove_column :groups, :user_ids, :integer
-  end
-end
